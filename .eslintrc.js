8605eef5beb07fe002109dee7c8e3e1bfc9a0dff
ifty_cloud <ifty_cloud@126.com>
2023-04-25 15:12:03 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/standard',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': [2, 'always-multiline'],
+    'comma-spacing': [2, {
+      before: false,
+      after: true,
+    }],
+    'comma-style': [2, 'last'],
+    semi: ['error', 'always'],
+    'semi-spacing': [2, {
+      before: false,
+      after: true,
+    }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+  },
+};
